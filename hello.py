dc609dedc4a9f39fc725a75b476f85088b3f182a
Harun-Ur-Rashid <98389165+harungt@users.noreply.github.com>
2022-10-24 10:57:33 +0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,29 @@
-def sum(x,y):
-    print("Total Sum")
-    return x+y
+import time   
 
-print(sum(6,8))
+final_list = []    
+
+def factorial(n):
+    time.sleep(.1)   
+
+    factorial = 1   
+
+    for i in range (1,n+1):
+        factorial = factorial * i   
+
+        return factorial     
+
+def sum_factorial():  
+
+    for i in range(50):   
+
+        final_list.append(factorial(i))    
+
+    result=sum(final_list)    
+
+    print("Final SUM is {}".format(result)) 
+
+    return result
+
+if __name__ == "__main__": 
+
+    sum_factorial()  
